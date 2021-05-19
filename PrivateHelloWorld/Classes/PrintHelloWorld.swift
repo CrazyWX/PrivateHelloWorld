34b2dd0f7da2def52d9ca999939e2f6c0ed11090
wangxia03 <wangxia03@huatu.com>
2021-05-19 11:44:40 +0800

    first commit

---
+++ PrivateHelloWorld/Classes/PrintHelloWorld.swift
@@ -0,0 +1,15 @@
+//
+//  PrintHelloWorld.swift
+//  HTOA
+//
+//  Created by 王霞 on 2021/5/19.
+//  Copyright © 2021 Personal. All rights reserved.
+//
+
+import UIKit
+
+class PrintHelloWorld: NSObject {
+    static func test() {
+        print("PrintHelloWorld ---------------------")
+    }
+}
